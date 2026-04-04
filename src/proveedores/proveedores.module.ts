4702cd4802196ac795dccd2bd642f1e18a1f66a6
cotanol <salcedomoralesfranciscojose@gmail.com>
2026-04-04 12:40:58 -0500

    fix add authmodule in proveedores

--- src/proveedores/proveedores.module.ts
+++ src/proveedores/proveedores.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProveedoresService } from './proveedores.service';
 import { ProveedoresController } from './proveedores.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProveedoresController],
   providers: [ProveedoresService],
   exports: [ProveedoresService],
